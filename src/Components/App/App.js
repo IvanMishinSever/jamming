89b62bacd16d68e0a45c27508caecc6b892ae2ed
Big ML <m@y.ru>
2020-11-10 20:06:05 +0300

    add save spotify 65

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,7 @@ export default class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
 
   }
   addTrack(track) {
@@ -45,6 +46,11 @@ export default class App extends React.Component {
     this.setState({playlistName: name});
   }
 
+  savePlaylist(){
+    alert('ok');
+   const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
   render() {
     return (
       <div>
@@ -54,7 +60,8 @@ export default class App extends React.Component {
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} 
-            onNameChange={this.updatePlaylistName} />
+            onNameChange={this.updatePlaylistName}
+            onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
