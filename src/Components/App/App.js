a23645337379e1b4b9d184d76170e4d83de4773a
Big ML <m@y.ru>
2020-11-10 18:41:28 +0300

    add change name playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,8 @@ export default class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -39,6 +41,9 @@ export default class App extends React.Component {
       this.setState({playlistTracks: tracks})
     
   }
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
 
   render() {
     return (
@@ -48,7 +53,8 @@ export default class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} 
+            onNameChange={this.updatePlaylistName} />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,11 +3,19 @@ import './Playlist.css';
 import  TrackList  from '../TrackList/TrackList';
 
 export default class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+    handleNameChange(event) {
+        this.props.onNameChange(event);
+    }
     render() {
         return (
             <div className="Playlist">
                 <input defaultValue={'New Playlist'}/>
-                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} />
+                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true} 
+                onChange={this.handleNameChange} />
                 <button className="Playlist-save">SAVE TO SPOTIFY</button>
             </div>
         )
