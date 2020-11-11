edd4b145626b67ac4382d910feb41255d5022ea8
Big ML <m@y.ru>
2020-11-11 12:46:02 +0300

    88

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,6 +5,8 @@ import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
+import Spotify from '../../util/Spotify';
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -52,7 +54,11 @@ export default class App extends React.Component {
    const trackURIs = this.state.playlistTracks.map(track => track.uri);
   }
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({
+        searchResults: searchResults
+      })
+    })
   }
   render() {
     return (
